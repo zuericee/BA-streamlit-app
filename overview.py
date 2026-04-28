298f08370b3d75f383bd8942b1f56101417613ca
zuericee <celine.schwarz@uzh.ch>
2026-04-28 14:17:48 +0200

    fix page setup

--- overview.py
+++ overview.py
@@ -1,13 +1,13 @@
 import streamlit as st
-import pandas as pd
-import openpyxl
 
 st.set_page_config(page_title="Bachelor Thesis", layout="wide")
 
-homepage = st.Page("pages/homepage.py", title="Fairness Judgments and the Roles of Opportunity, Risk-Taking, and Luck")
+# Define pages
+balancetable_page = st.Page("umfrage/pages/balancetable.py", title="Balance Table")
+powertest_page = st.Page("umfrage/pages/powertest.py", title="Power Test")
 
-balancetable_page = st.Page("pages/balancetable.py", title="Balance Table")
+# Create navigation
+pg = st.navigation([balancetable_page, powertest_page])
 
-powertest_page = st.Page("pages/powertest.py", title="Power Test")
-
-user_pages = [homepage, balancetable_page, powertest_page]
+# Run selected page
+pg.run()
